abcae07444fa1e6402851e2375caa57605c4c8e2
azzlabs <marco@azzlabs.eu>
2020-08-11 22:21:40 +0200

    Added yaml parser

--- inc/editor-page.inc.php
+++ inc/editor-page.inc.php
@@ -1,64 +1,42 @@
 <?php function mcit_editor_page() { ?>
 
-    <?php
+    <?php 
+        $mcit_editor = new MCIT_editor();
+        $mcit_editor->mcit_load_yaml_file(ABSPATH . get_option('mcit_server_info_path'));
+    ?>
 
-        $categories = [
-            'creative', 'ctf', 'factions', 'hardcore', 'hungergames', 'minigames', 'moddati', 'pixelmon', 
-            'prison', 'pvp', 'roleplay', 'skygames', 'survival', 'survivalgames', 'uhc', 'vanilla'
-        ];
-
-        $server_info_fields = [
-                ['slug' => 'name', 'name' => __('Nome del server', 'mcit'), 'desc' => __('Non deve contenere decorazioni o testo che non sia parte integrante del nome del server', 'mcit'), 'type' => 'text', 'length' => '24'],
-                ['slug' => 'url', 'name' => __('URL in Minecraft-Italia', 'mcit'), 'desc' => __('URL della pagina personalizzato', 'mcit'), 'type' => 'text', 'length' => '24'],
-                ['slug' => 'hidden', 'name' => __('Nascosto', 'mcit'), 'desc' => __('Indica se il server deve essere nascosto temporaneamente dalla lista', 'mcit'), 'type' => 'checkbox', 'length' => ''],
-                ['slug' => 'only-premium', 'name' => __('Solo premium', 'mcit'), 'desc' => __('Specifica se l\'accesso al server è riservato ai soli utenti possessori di una copia originale di Minecraft', 'mcit'), 'type' => 'checkbox', 'length' => ''],
-                ['slug' => 'versions', 'name' => __('Versioni supportate', 'mcit'), 'desc' => __('Elenco di versioni supportate dal server di Minecraft.<br>Usare solo versioni principali (es. 1.15). Se ha solo una versione, lasciare vuoto il campo "a"', 'mcit'), 'type' => 'text-from-to', 'length' => '4'],
-                ['slug' => 'categories', 'name' => __('Categorie', 'mcit'), 'desc' => __('Elenco di categorie di cui il server fa parte.<br>Ne vanno elencate un massimo di 5 e devono essere già attive al momento della modifica del file .yml<br>Suggerimento: usa ctrl + click per selezionarne più di una', 'mcit'), 'type' => 'select-multi', 'values' => $categories],
-                ['slug' => 'staff', 'name' => __('Staff', 'mcit'), 'desc' => __('Elenco di ruoli, contenenti a loro volta un elenco di Minecraft UUID relativi agli staffer del server.<br>Se non si vuole la scheda "Staff" basta lasciare il campo vuoto', 'mcit'), 'type' => 'repeater', 'length' => '36'],
-                ['slug' => 'www', 'name' => __('Sito web', 'mcit'), 'desc' => __('Indirizzo completo del sito web. Va preceduto da http:// o https://', 'mcit'), 'type' => 'text', 'length' => '48'],
-                ['slug' => 'forum', 'name' => __('Forum', 'mcit'), 'desc' => __('Indirizzo completo del forum. Va preceduto da http:// o https://', 'mcit'), 'type' => 'text', 'length' => '48'],
-                ['slug' => 'telegram', 'name' => __('Telegram', 'mcit'), 'desc' => __('Invito al canale Telegram. Va preceduto da http:// o https://', 'mcit'), 'type' => 'text', 'length' => '32'],
-                ['slug' => 'discord', 'name' => __('Discord', 'mcit'), 'desc' => __('Invito al server Discord. Va preceduto da http:// o https://', 'mcit'), 'type' => 'text', 'length' => '32'],
-                ['slug' => 'twitter', 'name' => __('Twitter', 'mcit'), 'desc' => __('Username del profilo di Twitter', 'mcit'), 'type' => 'text', 'length' => '15'],
-                ['slug' => 'instagram', 'name' => __('Instagram', 'mcit'), 'desc' => __('Username del profilo di Instagram', 'mcit'), 'type' => 'text', 'length' => '30'],
-                ['slug' => 'teamspeak', 'name' => __('TeamSpeak', 'mcit'), 'desc' => __('Indirizzo del server TeamSpeak', 'mcit'), 'type' => 'text', 'length' => '48'],
-                ['slug' => 'mumble', 'name' => __('Mumble', 'mcit'), 'desc' => __('Indirizzo del server Mumble', 'mcit'), 'type' => 'text', 'length' => '48'],
-                ['slug' => 'query_port', 'name' => __('Query port', 'mcit'), 'desc' => __('Indica se, per questioni tecniche, la server query va eseguita su una porta specifica', 'mcit'), 'type' => 'number', 'length' => ''],
-                ['slug' => 'theme_color', 'name' => __('Colore del tema', 'mcit'), 'desc' => __('Colore in esadecimale, indica il colore del tema della pagina in lista server', 'mcit'), 'type' => 'color-field', 'length' => '']
-            ];
+    <pre><?php 
         
-        function mcit_parse_server_info_field($field) {
-            switch ($field['type']) {
-                case 'repeater':
-                    return sprintf('<div id="staff-section"></div>
-                        <button class="button button-secondary addSection">Aggiungi sezione</button>', $field['slug'], $field['length']);
-                    break;
-                case 'text-from-to': return sprintf('%s <input type="text" name="%s-from" maxlength="%s"/> %s <input type="text" name="%s-to" maxlength="%s"/>', 
-                    __('da', 'mcit'), $field['slug'], $field['length'], __('a', 'mcit'), $field['slug'], $field['length']); break;
-                case 'select-multi': 
-                    $sel = sprintf('<select name="%s" class="regular-text" multiple>', $field['slug']);
-                    foreach ($field['values'] as $opt) {
-                        $sel.= sprintf('<option value="%s">%s</option>', $opt, ucfirst($opt));
-                    }
-                    return $sel . '</select>';
-                    break;
-                case 'color-field': return sprintf('<input class="color_field" type="text" name="%d" style="margin-right: .5em"/>', $field['slug']); break;
-                default: return sprintf('<input type="%s" name="%s" class="regular-text" maxlength="%s"/>', $field['type'], $field['slug'], $field['length']);
+        print_r($mcit_editor->current_dump->categories);
+        
+        if (!empty($_POST)) {
+            $yaml_file = '';
+
+            foreach ($mcit_editor->server_info_fields as $field) {
+                $yaml_file .= $mcit_editor->mcit_post_yaml_parser($field);
             }
+
+            file_put_contents(ABSPATH . get_option('mcit_server_info_path'), $yaml_file);
         }
+
+    ?></pre>
+
+    <?php
+        
+        
     ?>
 
     <div class="wrap">
         <h1>Editor <em>server-info.yml</em> per Minecraft-Italia.it</h1>
         
-        <form method="post" action="options.php">
+        <form method="post">
             <table class="form-table">
                 
-                <?php foreach($server_info_fields as $info_field) { ?>
+                <?php foreach($mcit_editor->server_info_fields as $info_field) { ?>
                 <tr valign="top">
                     <th scope="row"><?php echo $info_field['name'] ?></th>
                     <td style="padding-bottom: 0">
-                        <?php echo mcit_parse_server_info_field($info_field) ?>
+                        <?php echo $mcit_editor->mcit_parse_server_info_field($info_field) ?>
                         <p class="description">
                             <?php echo $info_field['desc'] ?>
                         </p>
@@ -67,22 +45,36 @@
                 
                 <?php } ?>
             </table>
-                    
+            
+            <?php submit_button(); ?>
         </form>
     </div>
 
     <script>
-        jQuery(document).ready(function($){
+        jQuery(document).ready(function($) {
+            var count = 0;
+
             $('.addSection').click(function(e) {
+                const slug = $(this).data('sectionslug');
+                count++;
+
                 e.preventDefault();
-                $('#staff-section').append(`<div class="staff-entry" style="margin-bottom: .5em"><input type="text" name="%s" class="regular-text" maxlength="%s" placeholder="<?php echo __('Nome sezione', 'mcit') ?>" />
-                    <button class="button button-secondary addEntry">Aggiungi staff</button> <button class="button-link button-link-delete delSection"><?php echo __('Rimuovi sezione', 'mcit') ?></button><div class="section-entries"></div></div>`);
+                $(this).parent().find('.parent-section').append(`<div class="${slug}-entry" style="margin-bottom: .5em">
+                        <input type="text" name="${slug}[${count}]" class="regular-text" maxlength="50" placeholder="<?php echo __('Nome sezione', 'mcit') ?>" />
+                        <button class="button button-secondary addEntry" data-sectionslug="${slug}_entry_${count}">Aggiungi ${slug}</button> 
+                        <button class="button-link button-link-delete delSection"><?php echo __('Rimuovi sezione', 'mcit') ?></button>
+                        <div class="section-entries"></div>
+                    </div>`);
             
                 $('.addEntry').unbind();
                 $('.addEntry').click(function(e) {
+                    const section_slug = $(this).data('sectionslug');
+
                     e.preventDefault();
-                    $(this).parent().find('.section-entries').append(`<div class="staff-entry" style="margin-top: .5em; margin-left: 1.2em"><input type="text" name="%s" class="regular-text" maxlength="%s" placeholder="<?php echo __('Nome staff', 'mcit') ?>" />
-                        <button class="button-link button-link-delete delSection"><?php echo __('Rimuovi', 'mcit') ?></button></div>`);
+                    $(this).parent().find('.section-entries').append(`<div class="${slug}-entry" style="margin-top: .5em; margin-left: 1.2em">
+                            <input type="text" name="${section_slug}[]" class="regular-text" maxlength="50" placeholder="<?php echo __('Nome', 'mcit') ?> ${slug}" />
+                            <button class="button-link button-link-delete delSection"><?php echo __('Rimuovi', 'mcit') ?></button>
+                        </div>`);
 
                     $('.delSection').unbind();
                     $('.delSection').click(function(e) {
---
+++ inc/mcit-editor.inc.php
@@ -0,0 +1,133 @@
+<?php
+class MCIT_editor {
+    public $categories;
+    public $server_info_fields;
+    public $current_dump;
+
+    public function __construct() {
+        $this->categories = [
+            'creative', 'ctf', 'factions', 'hardcore', 'hungergames', 'minigames', 'moddati', 'pixelmon', 
+            'prison', 'pvp', 'roleplay', 'skygames', 'survival', 'survivalgames', 'uhc', 'vanilla'
+        ];
+
+        $this->server_info_fields = [
+            ['slug' => 'name', 'name' => __('Nome del server', 'mcit'), 'desc' => __('Non deve contenere decorazioni o testo che non sia parte integrante del nome del server', 'mcit'), 'type' => 'text', 'length' => '24'],
+            ['slug' => 'url', 'name' => __('URL in Minecraft-Italia', 'mcit'), 'desc' => __('URL della pagina personalizzato', 'mcit'), 'type' => 'text', 'length' => '24'],
+            ['slug' => 'hidden', 'name' => __('Nascosto', 'mcit'), 'desc' => __('Indica se il server deve essere nascosto temporaneamente dalla lista', 'mcit'), 'type' => 'checkbox', 'length' => ''],
+            ['slug' => 'only-premium', 'name' => __('Solo premium', 'mcit'), 'desc' => __('Specifica se l\'accesso al server è riservato ai soli utenti possessori di una copia originale di Minecraft', 'mcit'), 'type' => 'checkbox', 'length' => ''],
+            ['slug' => 'versions', 'name' => __('Versioni supportate', 'mcit'), 'desc' => __('Elenco di versioni supportate dal server di Minecraft.<br>Usare solo versioni principali (es. 1.15). Se ha solo una versione, lasciare vuoto il campo "a"', 'mcit'), 'type' => 'text-from-to', 'length' => '4'],
+            ['slug' => 'categories', 'name' => __('Categorie', 'mcit'), 'desc' => __('Elenco di categorie di cui il server fa parte.<br>Ne vanno elencate un massimo di 5 e devono essere già attive al momento della modifica del file .yml<br>Suggerimento: usa ctrl + click per selezionarne più di una', 'mcit'), 'type' => 'select-multi', 'values' => $this->categories],
+            ['slug' => 'staff', 'name' => __('Staff', 'mcit'), 'desc' => __('Elenco di ruoli, contenenti a loro volta un elenco di Minecraft UUID relativi agli staffer del server.<br>Se non si vuole la scheda "Staff" basta lasciare il campo vuoto', 'mcit'), 'type' => 'repeater', 'length' => '36'],
+            ['slug' => 'www', 'name' => __('Sito web', 'mcit'), 'desc' => __('Indirizzo completo del sito web. Va preceduto da http:// o https://', 'mcit'), 'type' => 'text', 'length' => '48'],
+            ['slug' => 'forum', 'name' => __('Forum', 'mcit'), 'desc' => __('Indirizzo completo del forum. Va preceduto da http:// o https://', 'mcit'), 'type' => 'text', 'length' => '48'],
+            ['slug' => 'telegram', 'name' => __('Telegram', 'mcit'), 'desc' => __('Invito al canale Telegram. Va preceduto da http:// o https://', 'mcit'), 'type' => 'text', 'length' => '32'],
+            ['slug' => 'discord', 'name' => __('Discord', 'mcit'), 'desc' => __('Invito al server Discord. Va preceduto da http:// o https://', 'mcit'), 'type' => 'text', 'length' => '32'],
+            ['slug' => 'twitter', 'name' => __('Twitter', 'mcit'), 'desc' => __('Username del profilo di Twitter', 'mcit'), 'type' => 'text', 'length' => '15'],
+            ['slug' => 'instagram', 'name' => __('Instagram', 'mcit'), 'desc' => __('Username del profilo di Instagram', 'mcit'), 'type' => 'text', 'length' => '30'],
+            ['slug' => 'teamspeak', 'name' => __('TeamSpeak', 'mcit'), 'desc' => __('Indirizzo del server TeamSpeak', 'mcit'), 'type' => 'text', 'length' => '48'],
+            ['slug' => 'mumble', 'name' => __('Mumble', 'mcit'), 'desc' => __('Indirizzo del server Mumble', 'mcit'), 'type' => 'text', 'length' => '48'],
+            ['slug' => 'query_port', 'name' => __('Query port', 'mcit'), 'desc' => __('Indica se, per questioni tecniche, la server query va eseguita su una porta specifica', 'mcit'), 'type' => 'number', 'length' => ''],
+            ['slug' => 'theme_color', 'name' => __('Colore del tema', 'mcit'), 'desc' => __('Colore in esadecimale, indica il colore del tema della pagina in lista server', 'mcit'), 'type' => 'color-field', 'length' => '']
+        ];
+    }
+
+    public function mcit_load_yaml_file($filename) {
+        $yaml = new Dallgoot\Yaml\Loader();
+        $this->current_dump = $yaml->load($filename)->parse();
+    }
+
+    public function mcit_post_yaml_parser($field) {
+        if (!empty($_POST[$field['slug']]) || (!empty($_POST[$field['slug'] . '-from']) && !empty($_POST[$field['slug'] . '-to']))) {
+            switch ($field['type']) {
+                case 'text-from-to':
+                    $return_val = $field['slug'] . ":\n";
+                    $n_from = (int)substr($_POST[$field['slug'] . '-from'], 2);
+                    $n_to = (int)substr($_POST[$field['slug'] . '-to'], 2);
+                    for ($i = $n_from; $i <= $n_to; $i++) {
+                        $return_val .= sprintf("- \"%s\"\n", substr($_POST[$field['slug'] . '-from'], 0, 2) . $i);
+                    }
+                    return $return_val;
+                case 'select-multi': 
+                    $return_val = $field['slug'] . ":\n";
+                    foreach ($_POST[$field['slug']] as $entry_value) {
+                        $return_val .= sprintf("- \"%s\"\n", $entry_value);
+                    }
+                    return $return_val;
+                case 'repeater':
+                    $return_val = $field['slug'] . ":\n";
+                    foreach ($_POST[$field['slug']] as $key => $entry_label) {
+                        $return_val .= sprintf("- %s:\n", $entry_label);
+
+                        $values_slug = $field['slug'] . '_entry_' . $key;
+                        if (!empty($_POST[$values_slug])){
+                            foreach ($_POST[$values_slug] as $entry_value) {
+                                $return_val .= sprintf("  - \"%s\"\n", $entry_value);
+                            }
+                        }
+                    }
+                    return $return_val;
+                case 'checkbox': return sprintf("%s: %s\n", $field['slug'], str_replace('"', '\"', $_POST[$field['slug']]));
+                case 'number': return sprintf("%s: %d\n", $field['slug'], str_replace('"', '\"', (int)$_POST[$field['slug']]));
+                default: return sprintf("%s: \"%s\"\n", $field['slug'], str_replace('"', '\"', $_POST[$field['slug']]));
+            }
+        }
+    }
+
+    public function mcit_parse_server_info_field($field) {
+        switch ($field['type']) {
+            case 'repeater':
+                return sprintf('<div class="parent-section"></div>
+                    <button class="button button-secondary addSection" data-sectionslug="%s">Aggiungi sezione</button>', $field['slug'], $field['length'], $field['slug']);
+                break;
+            case 'text-from-to': return sprintf('%s <input type="text" name="%s-from" maxlength="%s"/> %s <input type="text" name="%s-to" maxlength="%s"/>', 
+                __('da', 'mcit'), $field['slug'], $field['length'], __('a', 'mcit'), $field['slug'], $field['length']); break;
+            case 'select-multi': 
+                $sel = sprintf('<select name="%s[]" class="regular-text" multiple>', $field['slug']);
+                foreach ($field['values'] as $opt) {
+                    $sel.= sprintf('<option value="%s">%s</option>', $opt, ucfirst($opt));
+                }
+                return $sel . '</select>';
+                break;
+            case 'color-field': return sprintf('<input class="color_field" type="text" name="%s" style="margin-right: .5em" value="%s"/>', $field['slug'], $this->mcit_get_cur_value($field)); break;
+            case 'checkbox': return sprintf('<input type="hidden" name="%s" value="false"/><input type="checkbox" name="%s" value="true" %s/>', $field['slug'], $field['slug'], $this->mcit_get_cur_value($field, false) ? 'checked' : '');
+            default: return sprintf('<input type="%s" name="%s" class="regular-text" maxlength="%s" value="%s"/>', $field['type'], $field['slug'], $field['length'], $this->mcit_get_cur_value($field));
+        }
+    }
+
+    public function mcit_get_cur_value($field, $default = '', $fromto = false) {
+        $current_dump = $this->current_dump;
+
+        if (!empty($current_dump[$field['slug']]) || (!empty($current_dump[$field['slug'] . '-from']) && !empty($current_dump[$field['slug'] . '-to']))) {
+            switch ($field['type']) {
+                case 'text-from-to':
+                    $return_val = $field['slug'] . ":\n";
+                    $n_from = (int)substr($current_dump[$field['slug'] . '-from'], 2);
+                    $n_to = (int)substr($current_dump[$field['slug'] . '-to'], 2);
+                    for ($i = $n_from; $i <= $n_to; $i++) {
+                        $return_val .= sprintf("- \"%s\"\n", substr($current_dump[$field['slug'] . '-from'], 0, 2) . $i);
+                    }
+                    return ($fromto ? '':'');// $return_val;
+                case 'select-multi': 
+                    $return_val = $field['slug'] . ":\n";
+                    foreach ($current_dump[$field['slug']] as $entry_value) {
+                        $return_val .= sprintf("- \"%s\"\n", $entry_value);
+                    }
+                    return '';// $return_val;
+                case 'repeater':
+                    $return_val = $field['slug'] . ":\n";
+                    foreach ($current_dump[$field['slug']] as $key => $entry_label) {
+                        $return_val .= sprintf("- %s:\n", $entry_label);
+
+                        $values_slug = $field['slug'] . '_entry_' . $key;
+                        if (!empty($current_dump[$values_slug])){
+                            foreach ($current_dump[$values_slug] as $entry_value) {
+                                $return_val .= sprintf("  - \"%s\"\n", $entry_value);
+                            }
+                        }
+                    }
+                    return '';// $return_val;
+                default: return $current_dump[$field['slug']];
+            }
+        } else return $default;
+    }
+}
